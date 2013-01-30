0a6d3f56f9170b8d1438ab41fbd0f0507b3df848
Tobi09 <t.lentz@gmx.net>
2013-01-30 13:57:41 +0100

    test

---
+++ HWR APP/Javascript/Funktionstests.js
@@ -0,0 +1,52 @@
+    function onDeviceReady() {
+		console.log("test on device ready");
+		alert("on device ready!!!!");
+		navigator.notification.alert("PhoneGap is working");
+    }
+
+var deviceReady = false;
+function init() {
+	console.log("init();");
+    document.addEventListener("deviceready", function () {
+    deviceReady = true;
+	console.log("deviceReady -> in init()");
+	$("#loginPage").on("pageinit",function() {
+	console.log("pageinit run");
+	$("#loginForm").on("submit",handleLogin);
+	checkPreAuth();
+	});
+	$.mobile.changePage("#loginPage");
+    }, false);
+
+    window.setTimeout(function () {
+        if (!deviceReady) {
+            alert("Error: Phonegap did not initialize.  Demo will not run correctly.");
+            console.log("Error: Phonegap did not initialize.  Demo will not run correctly.");
+        } else {
+			alert("Phonegap did initialize. Demo will run correctly.");
+            console.log("Phonegap did initialize. Demo not run correctly.");
+		}
+    }, 1000);
+} 
+
+    function showAlert() {
+        navigator.notification.alert(
+            'You are the winner!',  // message
+            'Game Over',            // title
+            'Done'                  // buttonName
+        );
+    }
+
+    // Beep three times
+    //
+    function playBeep() {
+        navigator.notification.beep(3);
+    }
+
+    // Vibrate for 2 seconds
+    //
+    function vibrate() {
+        navigator.notification.vibrate(2000);
+    }
+	
+	

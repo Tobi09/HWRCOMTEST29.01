5c991f8b67700a50ec6103b71918e52e28606674
Tobi09 <t.lentz@gmx.net>
2013-02-04 10:39:00 +0100

    test kontakte

--- HWR APP/Javascript/Funktionstests.js
+++ HWR APP/Javascript/Funktionstests.js
@@ -1,8 +1,15 @@
-var db;
 var deviceReady = false;
+
 function init() {
 	console.log("init();");
     document.addEventListener("deviceready", onDeviceReady(), false);
+	 if (!supportsLocalStorage()) { 
+	
+		navigator.notification.alert("DB Error: " + err, function() {}, "DB Error", "OK");
+		return false; 
+	} else {
+	 
+	 }
 
     window.setTimeout(function () {
         if (!deviceReady) {
@@ -11,67 +18,42 @@ function init() {
         } else {
 			//alert("Phonegap did initialize. Demo will run correctly.");
             console.log("Phonegap did initialize. Demo not run correctly.");
+
 			delete init;
 		}
     }, 1000);
-	
-	
-	
-	delete init();
+	delete init;
 } 
 
 function onDeviceReady() {
-    deviceReady = true;
-	//DB
 	console.log("onDeviceReady");
-	db = window.openDatabase("hwr-com-db", "1.0", "hwr-com-db", 100000);
-	db.transaction(populateDB, errorCB, successCB);	
+    deviceReady = true;
 	}
-
-	// Populate the database 
-    //
-    function populateDB(tx) {
-		console.log("populateDB");
-        tx.executeSql('DROP TABLE IF EXISTS DEMO');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
-        tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
-        tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
-		tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "third row")');
-		tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "fourth row")')	
-		tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "fiveth row")');
-    }
-
-    // Transaction error callback
-    //
-    function errorCB(tx, err) {
-		console.log("errorCB");
-       navigator.notification.alert("DB Error: " + err, function() {}, "DB Error", "OK");
-    }
-
-    // Transaction success callback
-    //
-    function successCB() {
-		console.log("successCB");
-        navigator.notification.alert("Succes DB Cennection", function() {}, "DB Connection", "OK");
-    }
 	
-	 //
-    function queryDB(tx) {
-		console.log("queryDB");
-        tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+function saveObjekt(name, value) {
+	window.localStorage.setItem("name", "value");
+}
+
+function loadObjekt(name) {
+	var value ) window.localStorage.getItem("name");
+	return value;
+}
+
+function deleteObjekt(name) {
+	window.localStorage.removeItem("name");
+}
+
+function saveKontaktList() {
+    var List = $('#List').html();
+    saveObjekt(Kontaktliste, Liste);
+}
+
+function loadKontaktList() {
+    var List = loadObjekt(Kontaktliste);
+    if (List != undefined) {
+        $('List').html(List);
     }
-	
-	function querySuccess(tx, results) {
-		console.log("querySuccess");
-		console.log("Returned rows = " + results.rows.length);
-		// this will be true since it was a select statement and so rowsAffected was 0
-		if (!results.rowsAffected) {
-			console.log('No rows affected!');
-			return false;
-	}
-	// for an insert statement, this property will return the ID of the last inserted row
-	console.log("Last inserted row ID = " + results.insertId);
-    }
+}
 
 	
 	//---------------------------------------------------//
